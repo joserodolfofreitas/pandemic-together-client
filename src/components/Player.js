0aef2f5ae73abbec2a16c3fffa82f999c8476038
Dominik Maier <dmaier@hirschsiegel.onmicrosoft.com>
2020-03-31 23:09:57 +0200

    card positioning

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,42 +4,35 @@ import { connect } from 'react-redux';
 
 function mapStateToProps(state) {
     return {
-        room : state.room,
-        roomState : state.roomState,
-        updatesOnRoomState : state.updatesOnRoomState,
+        room: state.room,
+        roomState: state.roomState,
+        updatesOnRoomState: state.updatesOnRoomState,
     }
 }
 
 class Player extends React.Component {
 
     render() {
-        let player = this.props.player;
-        let position = this.props.position;
-        let containerStyle = {gridArea: position};
-        let classes = `player ${position}${this.props.roomState.currentTurn == player.sessionId ? " current-turn" : ""}`;
-
-        var playerField = [player.advantages[0], player.disadvantages[0]];
-
+        const player = this.props.player;
+        const position = this.props.position;
+        const containerStyle = { gridArea: position };
+        const classes = `player ${position}${this.props.roomState.currentTurn == player.sessionId ? " current-turn" : ""}`;
+        const playerField = [player.advantages[0], player.disadvantages[0]];
+        const cardCount = player.virusField.length + 2; //2 playerField cards
         return (
             <div className={classes} style={containerStyle}>
-                <div style={{gridArea:"virus-infection", backgroundColor:"#00F", margin:"auto"}}>
-                    {player.virusField.map(function(card, index){
-                        return <Card key={card.cardId} card={card}/>
-                    })}
-
+                <div className="player-cards card-container" style={{ gridArea: "player-cards", "--card-count": cardCount }}>
+                    <div className="virus-infection" style={{ }}>
+                        {playerField.map((card, index) => <Card key={card.cardId} card={card} />)}
+                    </div>
+                    <div className="virus-infection" style={{ }}>
+                        {player.virusField.map((card, index) => <Card key={card.cardId} card={card} />)}
+                    </div>
                 </div>
-
-                <div style={{gridArea:"player-char", backgroundColor:"#F00", margin:"auto"}}>
-                    {playerField.map(function(card, index){
-                        return <Card key={card.cardId} card={card}/>
-                    })}
-                </div>
-                <div style={{gridArea:"player-name", backgroundColor:"#0F0", margin:"auto"}}>
-                    {player.name}
-                </div>
+                <div className="player-name" style={{ gridArea: "player-name" }}>{player.name}</div>
             </div>
         );
     }
 }
 
-export default connect(mapStateToProps, null) (Player)
+export default connect(mapStateToProps, null)(Player)
