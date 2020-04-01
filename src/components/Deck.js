672d0cefae815d7237f4abf27f01b6682eadc6b1
Dominik Maier <dmaier@hirschsiegel.onmicrosoft.com>
2020-04-01 02:07:46 +0200

    reducing card stack, as deck count goes down

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -13,13 +13,15 @@ class Deck extends React.Component {
         const currentPlayer = this.props.playerItems.filter(p => p.player.sessionId == this.props.roomState.currentTurn);
         const currentPlayerPosition = (currentPlayer && currentPlayer.length)?currentPlayer[0].position : "player-current";
         const className = `deck ${currentPlayerPosition}`;
-        if (this.props.roomState.deck.length > 0) {
+        const cardsOnStack = Math.min(this.props.roomState.deck.length, 4);
+        if (cardsOnStack > 0) {
+            let cards = [];
+            for(let i = 0; i < cardsOnStack; i++){
+                cards.push(<div className="card" style={{ backgroundImage: "url(/images/card-deck.png)" }} />)
+            }
             return <div className={className}>
                     <div className="stack">
-                        <div className="card" style={{ backgroundImage: "url(/images/card-deck.png)" }} />
-                        <div className="card" style={{ backgroundImage: "url(/images/card-deck.png)" }} />
-                        <div className="card" style={{ backgroundImage: "url(/images/card-deck.png)" }} />
-                        <div className="card" style={{ backgroundImage: "url(/images/card-deck.png)" }} />
+                        {cards}
                     </div>
                 </div>
         } else {
